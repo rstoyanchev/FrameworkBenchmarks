d60e8a0952d205e38d122ebd27282a9eeac93378
rstoyanchev <rossen.stoyanchev@broadcom.com>
2024-07-18 16:02:02 +0100

    Simplify routing

--- frameworks/Java/spring-webflux/src/main/java/benchmark/web/WebfluxRouter.java
+++ frameworks/Java/spring-webflux/src/main/java/benchmark/web/WebfluxRouter.java
@@ -1,23 +1,40 @@
 package benchmark.web;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 
 @Configuration
 public class WebfluxRouter {
 
-    @Bean
-    public RouterFunction<ServerResponse> route(WebfluxHandler handler) {
-        return RouterFunctions.route()
-                .GET("/plaintext", handler::plaintext)
-                .GET("/json", handler::json)
-                .GET("/db", handler::db)
-                .GET("/queries", handler::queries)
-                .GET("/updates", handler::updates)
-                .GET("/fortunes", handler::fortunes)
-                .build();
-    }
+	@Bean
+	public HttpHandler httpHandler(RouterFunction<ServerResponse> route, ServerFilter serverFilter) {
+		return WebHttpHandlerBuilder.webHandler(RouterFunctions.toWebHandler(route))
+				.filter(serverFilter)
+				.build();
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> route(WebfluxHandler handler) {
+		return request -> {
+			HandlerFunction<ServerResponse> fn = switch (request.uri().getRawPath()) {
+				case "/plaintext" -> handler::plaintext;
+				case "/json" -> handler::json;
+				case "/db" -> handler::db;
+				case "/queries" -> handler::queries;
+				case "/updates" -> handler::updates;
+				case "/fortunes" -> handler::fortunes;
+				default -> r -> ServerResponse.notFound().build();
+			};
+			return Mono.just(fn);
+		};
+	}
+
 }
